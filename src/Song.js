7cf35bfd92b36f35c4e55014728e68444f214618
Abdullahi Mahamed <abdullahimahamed42@gmail.com>
2018-06-26 17:21:54 -0500

    changing upvote icon color on click

--- src/Song.js
+++ src/Song.js
@@ -5,17 +5,17 @@ import FaSortUp from 'react-icons/lib/fa/chevron-up'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.playHandler = this.playHandler.bind(this)
   }
 
-  playHandler(){
-    this.props.playSong(this.props.track.album.uri,this.props.track.track_number - 1)
+  votedHandler() {
+    console.log("sd")
+    this.props.upVoteClicked(this.props.index)
   }
 
   render () {
     return (
-      <ListGroupItem className="justify-content-between song-container"  onClick={this.playHandler}>
-        <FaSortUp className="icon subdue"/>
+      <ListGroupItem className="justify-content-between song-container" >
+        <FaSortUp className="icon subdue" style={{color:(this.props.voted != false) ? "#2ecc71 ": "#A4A5A5"}} onClick={this.votedHandler.bind(this)} />
         <div className="song">
           <p className="song-title">{this.props.track.name}</p>
           <p className="subdue">{this.props.track.artists[0].name}</p>
