849f03e6f35febe84776e8cd9e625d1bb5b1dbe1
Abdullahi Mahamed <abdullahimahamed42@gmail.com>
2018-06-26 17:20:56 -0500

    queing next song in list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { ListGroup, ListGroupItem, Badge} from 'reactstrap'
 import FaSortUp from 'react-icons/lib/fa/chevron-up'
+import FaMusic from 'react-icons/lib/fa/music'
 import Spotify from 'spotify-web-api-js'
 import Song from './Song.js'
 
@@ -10,53 +11,111 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      songs: []
+      songs: [],
+      currentSong:null
     }
     this.spotify = new Spotify()
-    this.spotify.setAccessToken('BQANmpPhw1WL0F-gZ9WffF_Xu0J29sxC8qAwJHUALijgYtoR9AgZrwgBYyLpDOTdGUcLwUpjUsjxgo-AaxJrOhFGwY2fhWDVdKHAUYccsyfqDsrdm8XMBgTHU42Mon8ooPE5MT5tW-CTpx7V29u2PqiNJMob60I7XbZFRAlt5EOaTw0Myd9RFgHoQ78unwUbM4YSq7lSdEXD6hJFLVtGrsTf_LU63zzutdRWilC3pBqEIwV_vzAogXtHwzU1_IqjAB48V0_mcg');
-    this.playSong = this.playSong.bind(this)
+    this.spotify.setAccessToken('BQC2yMLlQ5Zhd3N1Gi_Oe4RuSNddq-0UqJAIjni-YN8-shqva-IYwbUVyO0zlT6c43gVxSLn4HYZ8Av1ejiX699wueJHbcH5_PrIFgsKkM-aSRwx50sMJnNmy6bLXBxlchhMPUg-q0rRATOCXY3vrWaHTvlkyfRdPZMYt6q9n9-bYU0opxawrplh5zuhDsIoSBqV3LN1fPocTmzk0GZQCAdVoiKENbqnMYx78yYUtT1rc_r2eWiyupNzAqRRv5no_mT34bX_WA');
+    this.playSongs = this.playSongs.bind(this)
+    this.upVote = this.upVote.bind(this)
   }
 
   componentDidMount(){
     this.spotify.getPlaylist('ook42', '37i9dQZF1DZ06evO1kxsTC').then(data => {
       var songs = data.tracks.items
+      var songs = songs.map((spotifySong,index) => {
+        var song = {}
+        song.spotify = spotifySong
+        song.votes = 0
+        song.voted = false
+        return song;
+      })
+
       this.setState({songs})
+      this.sortQue()
+      this.playSongs()
     });
+
   }
 
-  playSong(uri,offset){
+  sortQue(){
+    function sortVotes(a,b){
+      return b.votes - a.votes
+    }
+    var a = this.state.songs;
+    a.sort(sortVotes)
+    var swapped;
+
+    this.setState({songs:a})
+  }
+
+  playSongs(){
+    this.setState({currentSong:this.state.songs[0]});
+    var tempSongs = this.state.songs;
+    tempSongs.shift()
+    this.setState({songs:tempSongs})
     this.spotify.play({
-      context_uri:uri,
+      context_uri:this.state.currentSong.spotify.track.album.uri,
       offset:{
-        position:offset
+        position:this.state.currentSong.spotify.track.track_number - 1
       }
     },(error) => {
+      if(error){
+        alert("Could not play songs")
+      } else {
+        setTimeout(() => {
+          this.playSongs()
+        },10000,/*this.state.songs[0].spotify.track.duration_ms*/)
+      }
     })
   }
 
+  upVote(index){
+    var temp = this.state.songs;
+
+    if(temp[index].voted){
+      temp[index].votes -= 1
+    }
+    else {
+      temp[index].votes += 1
+    }
+
+    temp[index].voted = !temp[index].voted;
+    this.setState({songs:temp})
+    this.sortQue()
+  }
 
   renderSongs(){
     return this.state.songs.map((song,index) => {
       return (
-        <ListGroup className="group" key={index}>
-          <Song track={song.track} playSong={this.playSong}/>
+        <ListGroup key={index}>
+          <Song track={song.spotify.track} voted={song.voted} index={index} upVoteClicked={this.upVote} />
         </ListGroup>
       )
     });
+  }
+
+  renderCurrentSong() {
+    if(this.state.currentSong != null){
+      return (
+        <ListGroup className="group sticky">
+          <ListGroupItem className="justify-content-between song-container">
+            <div className="song current-song">
+              <FaMusic className="icon subdue" style={{color:(this.props.voted != false) ? "#2ecc71 ": "#A4A5A5"}} />
+              <p className="song-title">{this.state.currentSong.spotify.track.name}</p>
+              <p className="subdue">{this.state.currentSong.spotify.track.artists[0].name}</p>
+            </div>
+          </ListGroupItem>
+        </ListGroup>
+      )
+    }
   }
 
   render() {
     return (
       <div className="app">
         {this.renderSongs()}
-        <ListGroup className="group sticky">
-          <ListGroupItem className="justify-content-between song-container">
-            <div className="song current-song">
-              <p className="song-title">DNA</p>
-              <p className="subdue">Kendrick Lamar</p>
-            </div>
-          </ListGroupItem>
-        </ListGroup>
+        {this.renderCurrentSong()}
       </div>
     );
   }
