362257a395bcafa029a1d13292d0fc98e07fad8f
abdullahi <abdullahimahamed0987@gmail.com>
2018-07-08 18:27:31 -0500

    added badge and song thumbnail

--- src/Song.js
+++ src/Song.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
-import { ListGroupItem } from 'reactstrap'
+import { ListGroupItem,Badge,Container, Row, Col,Media  } from 'reactstrap'
 import FaSortUp from 'react-icons/lib/fa/chevron-up'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.playHandler = this.playHandler.bind(this)
+    this.voteHandler = this.voteHandler.bind(this)
   }
 
-  playHandler(){
-    this.props.playSong(this.props.track.album.uri,this.props.track.track_number - 1)
+  voteHandler(){
+    this.props.upVoteSong(this.props.index)
   }
 
   render () {
     return (
-      <ListGroupItem className="justify-content-between song-container"  onClick={this.playHandler}>
-        <FaSortUp className="icon subdue"/>
-        <div className="song">
-          <p className="song-title">{this.props.track.name}</p>
-          <p className="subdue">{this.props.track.artists[0].name}</p>
-        </div>
+      <ListGroupItem className="item-container">
+        <Container>
+          <Row>
+            <Badge className="thumbnail-badge">{this.props.song.votes}</Badge>
+            <Col xs="2" className="thumbnail" >
+              <img src={this.props.song.spotify.track.album.images[2].url} alt=""/>
+            </Col>
+            <Col xs="8">
+              <div className='song-container'>
+                <div className="song">
+                  <p className="song-title">{this.props.song.spotify.track.name}</p>
+                  <p className="subdue">{this.props.song.spotify.track.artists[0].name}</p>
+                </div>
+              </div>
+            </Col>
+            <Col xs="2">
+              <div className="icon" onClick={this.voteHandler}>
+                <FaSortUp className="icon subdue" style={{color:this.props.song.voted ? "#2ecc71":"#A4A5A5"}}/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </ListGroupItem>
     );
   }
 
 }
+
+// <p className="align-middle song-badge">
+//   <Badge pill>{this.props.song.votes}</Badge>
+// </p>
+
 
 export default App;
